b13b06bca1d9cda8b4c795cbfd0152a17dc843f9
WalterMarH <83478583+WalterMarH@users.noreply.github.com>
2022-05-18 12:21:18 -0300

    l

--- Ejercitacion/EJE-MOD4-Seg/ajusteimagen.py
+++ Ejercitacion/EJE-MOD4-Seg/ajusteimagen.py
@@ -57,7 +57,7 @@ def ajustarGamma(image, gamma):
 
 def aplicarKernel(image, kernel):
     """Doc..."""
-
+    contador=0
     imagenProcesada=copy.deepcopy(image)
     c=kernel[0][0]+kernel[0][1]+kernel[0][2]+kernel[1][0]+kernel[1][1]+kernel[1][2]+kernel[2][0]+kernel[2][1]+kernel[2][2]
     if c==0:
@@ -84,29 +84,30 @@ def aplicarKernel(image, kernel):
                 p21=(imagenProcesada.image[i+1,j,canal])*kernel[2][1]
                 p22=(imagenProcesada.image[i+1,j+1,canal])*kernel[2][2]
                 pixel = (p00+p01+p02+p10+p11+p12+p20+p21+p22)//c
-                print(pixel,"anterior")
+                contador=contador+1
                 if pixel < 0:
                     pixel = 0
                 elif pixel > 255:
                     pixel = 255
                 pixel=int(pixel)
-                print(pixel,"despues")
+                print(contador)
                 imagenProcesada.image[i][j][canal] = pixel
     return imagenProcesada
 
 def main():
     #Mostrar su implementación aquí
     #imagen2=Image((8,8,3))
-    imagen=Image(filename='spacex.jpg')
+    imagen=Image(filename='bote.jpg')
     #brillosa2=
     #brilloajustado=ajustarBrillo(imagen,1.5)
     #amostrar=plt.imshow(brillosa2)
 
     #brilloajustado.showImage()
     #contrastada=ajustarContraste(imagen,0.7)
-    #imagen.showImage()
+    imagen.showImage()
     #contrastada.showImage()
-    kernel=[[0,-1,0],[-1,5,-1],[0,0.-1,0]]
+    #kernel=[[0.0625,0.125,0.0625],[0.125,0.25,0.125],[0.0625,0.125,0.0625]] #blur
+    kernel=[[0,-1,0],[-1,8,-1],[0,-1,0]] #sharpen
     #print(kernel[0][0]+kernel[0][1]+kernel[0][2]+kernel[1][0]+kernel[1][1]+kernel[1][2]+kernel[2][0]+kernel[2][1]+kernel[2][2])
     kerneleada=aplicarKernel(imagen,kernel)
     kerneleada.showImage()
