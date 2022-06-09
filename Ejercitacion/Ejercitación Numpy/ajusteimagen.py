7915f173499f7ee8a4f1f009a75a29279ee5fb5a
WalterMarH <83478583+WalterMarH@users.noreply.github.com>
2022-06-09 01:37:23 -0300

    ñ

--- Ejercitacion/Ejercitación Numpy/ajusteimagen.py
+++ Ejercitacion/Ejercitación Numpy/ajusteimagen.py
@@ -34,17 +34,17 @@ def ajustarBrillo(imagene, ajuste=1):
 
 def getHistograma(image):
     """Recibe un objeto de la clase Imagen, retorna una matriz 256x3 con los valores del histpgrama y también muestra 3 figuras con los 3 histogramas correspondientes"""
-    # canales=getChannels(image)
+    #canales=getChannels(image)
     lista=np.arange(0,256,1)
-    historojo=np.histogram2d(image.image[0],bins=lista)
-    histoverde=np.histogram(image.image[1],bins=lista)
-    histoazul=np.histogram(image.image[2],bins=lista)
+    historojo, bins=np.histogram(image.image[:,:,0], bins=256, range=(0, 255))
+    histoverde, bins=np.histogram(image.image[:,:,1], bins=256, range=(0, 255))
+    histoazul, bins=np.histogram(image.image[:,:,2], bins=256, range=(0, 255))
 
 
-    listarojos=historojo[0]
-    listaverdes=histoverde[0]
-    listaazules=histoazul[0]
-    print(listarojos)
+    listarojos=historojo
+    listaverdes=histoverde
+    listaazules=histoazul
+    
     # listarojos=np.histogram(canales[3])
     # listaverdes=np.histogram(canales[4])
     # listaazules=np.histogram(canales[5])
@@ -71,22 +71,22 @@ def getHistograma(image):
     #                 listaazules[i]=listaazules[i]+1
     #             contador=contador+1
     #             #print(contador)
-    # figrojo = plt.figure(figsize = (10, 5))
-    # plt.bar(lista, listarojos, color ='red')
-    # plt.xlabel("0 Negro, 255 Blanco")
-    # plt.ylabel("Cantidad de veces que se repite")
-    # plt.title("Histograma Rojo")
-    # figverde = plt.figure(figsize = (10, 5))
-    # plt.bar(lista, listaverdes, color ='green')
-    # plt.xlabel("0 Negro, 255 Blanco")
-    # plt.ylabel("Cantidad de veces que se repite")
-    # plt.title("Histograma Verde")
-    # figrazul= plt.figure(figsize = (10, 5))
-    # plt.bar(lista, listaazules, color ='blue')
-    # plt.xlabel("0 Negro, 255 Blanco")
-    # plt.ylabel("Cantidad de veces que se repite")
-    # plt.title("Histograma Azul")
-    # plt.show()
+    figrojo = plt.figure(figsize = (10, 5))
+    plt.bar(lista, listarojos, color ='red')
+    plt.xlabel("0 Negro, 255 Blanco")
+    plt.ylabel("Cantidad de veces que se repite")
+    plt.title("Histograma Rojo")
+    figverde = plt.figure(figsize = (10, 5))
+    plt.bar(lista, listaverdes, color ='green')
+    plt.xlabel("0 Negro, 255 Blanco")
+    plt.ylabel("Cantidad de veces que se repite")
+    plt.title("Histograma Verde")
+    figrazul= plt.figure(figsize = (10, 5))
+    plt.bar(lista, listaazules, color ='blue')
+    plt.xlabel("0 Negro, 255 Blanco")
+    plt.ylabel("Cantidad de veces que se repite")
+    plt.title("Histograma Azul")
+    plt.show()
 
 
     # return [listarojos,listaverdes,listaazules]
@@ -139,24 +139,26 @@ def ajustarContraste(image, alfa):
 
 def ajustarGamma(image, gamma):
     """ Recive un objeto de la clase Image y un valor de ajuste, retorna un objeto de la clase Image con la gamma ajustada según el valor pasado"""
-    maximo=0
+    max=0
     imagenProcesada=copy.deepcopy(image)
-    for canal in range(3):
-        for i in range(imagenProcesada.fil):
-            for j in range(imagenProcesada.col):
-                if imagenProcesada.image[i][j][canal]>maximo:
-                    maximo=imagenProcesada.image[i][j][canal]
-    print(maximo)
-    contador=0
-    c=255/((maximo)**gamma)
-
-    for canal in range(3):
-        for i in range(imagenProcesada.fil):
-            for j in range(imagenProcesada.col):
-                pixel=c*(imagenProcesada.image[i][j][canal])**gamma
+    max=imagenProcesada.image.max()
+    #for canal in range(3):
+     #   for i in range(imagenProcesada.fil):
+      #      for j in range(imagenProcesada.col):
+       #         if imagenProcesada.image[i][j][canal]>maximo:
+        #            maximo=imagenProcesada.image[i][j][canal]
+    #print(maximo)
+    #contador=0
+    c=255/((max)**gamma)
+    arraygamma=np.full((image.fil,image.col,3), gamma)
+    #for canal in range(3):
+     #   for i in range(imagenProcesada.fil):
+      #      for j in range(imagenProcesada.col):
+       #         pixel=c*(imagenProcesada.image[i][j][canal])**gamma
                 #print(contador)
-                contador=contador+1
-                imagenProcesada.image[i][j][canal] = pixel
+        #        contador=contador+1
+         #       imagenProcesada.image[i][j][canal] = pixel
+    imagenProcesada.image=c*np.power(imagenProcesada.image,arraygamma)
     return imagenProcesada
 
 def aplicarKernel(image, kernel):
@@ -166,32 +168,40 @@ def aplicarKernel(image, kernel):
     c=kernel[0][0]+kernel[0][1]+kernel[0][2]+kernel[1][0]+kernel[1][1]+kernel[1][2]+kernel[2][0]+kernel[2][1]+kernel[2][2]
     if c==0:
         c=1
+    a=0
+    #array=[[a[i-1,j-1,canal] , a[i-1,j,canal],a[i-1,j+1,canal] ],[a[i,j-1,canal],a[i,j,canal] ,a[i,j+1,canal]],[a[i+1,j-1,canal],a[i+1,j,canal] ,a[i+1,j+1,canal]]]
     for canal in range(3):
         for i in range(1,imagenProcesada.fil-1):
             for j in range(1,imagenProcesada.col-1):
-                p00=(imagenProcesada.image[i-1,j-1,canal])*kernel[0][0]
-                p01=(imagenProcesada.image[i-1,j,canal])*kernel[0][1]
-                p02=(imagenProcesada.image[i-1,j+1,canal])*kernel[0][2]
-                p10=(imagenProcesada.image[i,j-1,canal])*kernel[1][0]
-                p11=(imagenProcesada.image[i,j,canal])*kernel[1][1]
-                p12=(imagenProcesada.image[i,j+1,canal])*kernel[1][2]
-                p20=(imagenProcesada.image[i+1,j-1,canal])*kernel[2][0]
-                p21=(imagenProcesada.image[i+1,j,canal])*kernel[2][1]
-                p22=(imagenProcesada.image[i+1,j+1,canal])*kernel[2][2]
-                pixel = (p00+p01+p02+p10+p11+p12+p20+p21+p22)//c
-                contador=contador+1
+                a=imagenProcesada.image
+                array=[[a[i-1,j-1,canal] , a[i-1,j,canal],a[i-1,j+1,canal] ],[a[i,j-1,canal],a[i,j,canal] ,a[i,j+1,canal]],[a[i+1,j-1,canal],a[i+1,j,canal] ,a[i+1,j+1,canal]]]
+                array=np.array(array)
+                kernel=np.array(kernel)
+                suma=np.sum(array*kernel)
+                #p00=(imagenProcesada.image[i-1,j-1,canal])*kernel[0][0]
+                #p01=(imagenProcesada.image[i-1,j,canal])*kernel[0][1]
+                #p02=(imagenProcesada.image[i-1,j+1,canal])*kernel[0][2]
+                #p10=(imagenProcesada.image[i,j-1,canal])*kernel[1][0]
+                #p11=(imagenProcesada.image[i,j,canal])*kernel[1][1]
+                #p12=(imagenProcesada.image[i,j+1,canal])*kernel[1][2]
+                #p20=(imagenProcesada.image[i+1,j-1,canal])*kernel[2][0]
+                #p21=(imagenProcesada.image[i+1,j,canal])*kernel[2][1]
+                #p22=(imagenProcesada.image[i+1,j+1,canal])*kernel[2][2]
+                #pixel = (p00+p01+p02+p10+p11+p12+p20+p21+p22)//c
+                pixel=suma//c
+                #contador=contador+1
                 if pixel < 0:
                     pixel = 0
                 elif pixel > 255:
                     pixel = 255
-                pixel=int(pixel)
+                #pixel=int(pixel)
                 #print(contador)
                 imagenProcesada.image[i][j][canal] = pixel
     return imagenProcesada
 
 def aplicarLog(image):
     """ Retorna un objeto de la clase imagen transformado con una función logarítmmica"""
-    maximo=0
+    max=0
     imagenProcesada=copy.deepcopy(image)
     max=imagenProcesada.image.max()
     # for canal in range(3):
@@ -229,19 +239,19 @@ def main():
     #brilloajustado.showImage()
     #contrastada=ajustarContraste(imagen,2)
     #contrastada.showImage()
-    # kernel=[[0.0625,0.125,0.0625],[0.125,0.25,0.125],[0.0625,0.125,0.0625]] #blur
+    kernel=[[0.0625,0.125,0.0625],[0.125,0.25,0.125],[0.0625,0.125,0.0625]] #blur
     # #kernel=[[0,-1,0],[-1,8,-1],[0,-1,0]] #sharpen
     # #print(kernel[0][0]+kernel[0][1]+kernel[0][2]+kernel[1][0]+kernel[1][1]+kernel[1][2]+kernel[2][0]+kernel[2][1]+kernel[2][2])
-    # kerneleada=aplicarKernel(imagen,kernel)
-    # kerneleada.showImage()
+    kerneleada=aplicarKernel(imagen,kernel)
+    kerneleada.showImage()
     # #print(kernel[2][2])
-    # gammaajustada=ajustarGamma(imagen,2)
-    # gammaajustada.showImage()
+    #gammaajustada=ajustarGamma(imagen,2)
+    #gammaajustada.showImage()
     # canales=getChannels(imagen)
     # canales[0].showImage()
     # canales[1].showImage()
     # canales[2].showImage()
-    histo=getHistograma(imagen)
+    #histo=getHistograma(imagen)
     # print(histo)
     # print(np.shape(histo))
     # logaritmeada=aplicarLog(imagen)
